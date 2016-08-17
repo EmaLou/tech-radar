d6ccca211e17a4d29e1c2011c09d6f1398468e62
LouJia <loujia1991@gmail.com>
2016-08-17 12:43:55 +0800

    modify data for Resi

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,6 @@
 $(function () {
     $.getJSON('data/resi-skillsets.json', function(data) {
-        ['Front-end', 'Back-end', 'OPS', 'QA'].forEach(function(category) {
+        ['Front-end', 'Back-end', 'OPS', 'QA', 'Mobile'].forEach(function(category) {
             generateChart(data, category);
         });
         generateBubble({children: flatten(data)});
